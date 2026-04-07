a77072da403c74097e56a1b15bc0720a021d8b15
AlonaHutsol <gucolalona636@gmail.com>
2026-04-07 11:50:53 +0300

    Add files via upload

---
+++ Laba7zav2/Laba7zav2/Laba7zav2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+const int rows = 5;
+const int cols = 5;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    ifstream inFile("2D.txt");
+
+    if (!inFile) {
+        cerr << "Не вдалося відкрити файл для читання." << endl;
+        return 1;
+    }
+
+    int matrix[rows][cols];
+
+    // Зчитування з файлу
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            inFile >> matrix[i][j];
+        }
+    }
+
+    inFile.close();
+
+    cout << "Матриця:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Пошук мінімального елемента головної діагоналі
+    int min = matrix[0][0];
+
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][i] < min) {
+            min = matrix[i][i];
+        }
+    }
+
+    cout << "Мінімальний елемент головної діагоналі: " << min << endl;
+
+    return 0;
+}
---
+++ laba7/laba7/laba7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    ifstream inputFile("number.txt");
+
+    if (!inputFile) {
+        cerr << "Не вдалося відкрити файл для читання." << endl;
+        return 1;
+    }
+
+    double number;
+    double dob = 1;
+
+    while (inputFile >> number) {
+        if ((int)number % 4 == 0) {   
+            dob *= number;
+        }
+    }
+
+    inputFile.close();
+
+    if (dob == 1)
+        cout << "В файлі не було чисел, кратних 4." << endl;
+    else
+        cout << "Добуток чисел, кратних 4 у файлі: " << dob << endl;
+
+    return 0;
+}
